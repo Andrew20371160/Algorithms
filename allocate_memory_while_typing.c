9463829dac0c52ca620a0f0c790b3c185987dd0a
Andrew Karam Saber <androw.20371160@eng.aun.edu.eg>
2023-08-26 16:54:37 +0300

    Update allocate_memory_while_typing.c

--- allocate_memory_while_typing.c
+++ allocate_memory_while_typing.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+//Send Null pointer and this function will allocate memory while user is typing
+char * get_input(char*str){
+str =malloc(sizeof(char)) ;
+*str = '\0' ;
+unsigned int i = 0 ;
+while((*(str+i)=getchar())!='\n'){
+    i++;
+    str = realloc(str,sizeof(char)*(i+1));
+}
+*(str+i)='\0';
+return str ;
+}
 
 int main()
 {
-char * str = NULL ;
-str = (char*)malloc(2*sizeof(char)) ;
-*(str+1) = '\0' ;
-int i = 0 ;
-while(*(str+i-1)!='x'){
-scanf("%c",str+i) ;
-
-i++ ;
-str = (char*)realloc(str,2+i);
-
-}
-printf("\n%s",str);
-free(str) ;
-str = NULL ;
+char *str = NULL ;
+str  =get_input(str);
+printf("\n%s",str) ;
+free(str) ;str = NULL ;
     return 0;
 }
