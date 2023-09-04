e8135967fa48f0e91be0bcbe316d89ceae2b996a
Andrew Karam Saber <androw.20371160@eng.aun.edu.eg>
2023-09-04 18:11:13 +0300

    Update MergeSort(linked_lists).c

--- MergeSort(linked_lists).c
+++ MergeSort(linked_lists).c
@@ -1,105 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
-//node structure
-typedef struct {
-int data ;
-struct node * next ;
-}node;
-//get memory for new node function
-node * get_node(int d){
-node * new_node = malloc(sizeof(node)) ;
-new_node->next = NULL;
-new_node->data=  d ;
-return new_node ;
-}
-//insertion function
-node * insert(int d , node * head){
-if(head==NULL){
-    head= get_node(d) ;
-}
-else {
-    node*ptr = head ;
-    while(ptr->next!=NULL){
-        ptr = ptr->next ;
-    }
-    node *new_node = get_node(d);
-    ptr->next = new_node;
-}
-return head ;
-}
-//display elements of a list
-void show(node*head){
-printf("\n");
-while(head!=NULL){
-    printf("%d ",head->data);
-    head = head->next;
-}
-}
-//get size of a linked list
-int listsize(node *head){
-int size = 0 ;
-while(head!=NULL){
-    head = head->next;
-    size++;
-}
-return size ;
-}
-//MergeSort function
-void merge(node*left , node*right ,node**head){
-node*pl = left ;
-node*pr = right ;
-node*ptr = *head ;
-while(pl!=NULL&&pr!=NULL){
-    if(pl->data<=pr->data){
-        ptr->data = pl ->data ;
-        ptr = ptr->next ;
-        pl = pl->next;
+void merge(node *left ,node *right,node **head){
+node *pleft = left ;
+node *pright = right ;
+node *phead = *head ;
+while(pleft&&pright){
+    if(pleft->data<pright->data){
+        phead->data = pleft->data ;
+        pleft= pleft->next;
     }
     else{
-        ptr->data =  pr->data ;
-        ptr = ptr->next ;
-        pr=pr->next ;
+        phead->data =pright->data ;
+        pright= pright->next ;
     }
+    phead=  phead->next ;
 }
-while(pl!=NULL){
-    ptr->data = pl->data ;
-    ptr = ptr->next ;
-    pl = pl->next;
+while(pleft){
+    phead->data = pleft->data ;
+    phead =phead->next ;
+    pleft  = pleft->next ;
 }
-while(pr!=NULL){
-    ptr->data = pr->data ;
-    ptr = ptr->next ;
-    pr = pr->next;
+while(pright){
+    phead->data = pright->data ;
+    phead = phead ->next ;
+    pright=  pright->next ;
 }
+delall(&left) ;
+delall(&right) ;
+}
+void merge_sort(node **head){
+uint32 size  = list_size(*head);
+if(size>1){
+node *left = NULL;
+node *right = NULL ;
+node *phead= *head ;
+for(uint32 i= 0 ; i<size/2;i++){
+add_at_beg(&left,phead->data);
+phead= phead->next;
 }
 
-void mergesort(node**head){
-int size = listsize(*head);
-if(size<2){
-    return ;
+for(uint32 i=size/2; i<size; i++){
+add_at_beg(&right,phead->data);
+    phead=phead->next ;
 }
-node *left = NULL ;
-node *right = NULL ;
-node*ptr = *head ;
-for(int i =0 ; i<size/2;i++ ){
-    left = insert(ptr->data,left) ;
-    ptr = ptr->next ;
+merge_sort(&left);
+merge_sort(&right);
+merge(left,right,head) ;
 }
-for(int i =size/2 ; i<size;i++ ){
-    right = insert(ptr->data,right) ;
-    ptr = ptr->next ;
 }
-mergesort(&left);
-mergesort(&right);
-merge(left,right,head);
+void delall(struct node **head){
+    node * second = (*head)->next ;
+    while(second!=NULL){
+    free(*head) ;
+    *head = NULL ;
+    *head = second ;
+    second= second->next ;
 }
-int main()
-{
-node *head = NULL;
-for(int i = 10 ; i >0 ; i--){
-    head=insert(i,head) ;
+if(*head){
+free(*head) ; *head = NULL ;
 }
-mergesort(&head);
-show(head);
-    return 0;
 }
